d39aa76fd51733acf2da6e516c0519ff46c1e25d
andrewzhao15 <andrew.zhao@windsurf.com>
2025-07-24 16:24:20 -0700

    Add Person model class with Jackson annotations

---
+++ src/main/java/com/example/model/Person.java
@@ -0,0 +1,56 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Person model class for Jackson Databind serialization/deserialization example.
+ */
+public class Person {
+    private String name;
+    private int age;
+    private String email;
+
+    // Default constructor required by Jackson
+    public Person() {
+    }
+
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    @JsonProperty("full_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
